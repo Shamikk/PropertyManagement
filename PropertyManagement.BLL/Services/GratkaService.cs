2f3cc3d01d987a370c0f16ea220f9fe0d88f92ef
Mikolaj <mikolaj.bednarek@gmail.com>
2020-03-22 12:51:52 +0100

    Created GratkaService class

---
+++ PropertyManagement.BLL/Services/GratkaService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PropertyManagement.BLL.Dtos;
+using PropertyManagement.BLL.Entities;
+
+namespace PropertyManagement.BLL.Services
+{
+    public class GratkaService : IGratkaOfferApi
+    {
+        // create method here
+
+
+        public void Send(GratkaOfferDto offer)
+        {
+            // here is the code that validates and sends data to gratka portal.
+        }
+    }
+}
