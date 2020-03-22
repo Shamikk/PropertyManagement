a63ef0e47d6068f4fdbf61bef954bc7281f9ebe6
Mikolaj <mikolaj.bednarek@gmail.com>
2020-03-22 16:41:56 +0100

    Some new stuff added

---
+++ PropertyManagement.BLL/Entities/Offer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyManagement.BLL.Entities
+{
+    public class Offer
+    {
+        public Human Bidder { get; set; }
+        public House House { get; set; }
+        public float OfferedPrice { get; set; }
+    }
+}
