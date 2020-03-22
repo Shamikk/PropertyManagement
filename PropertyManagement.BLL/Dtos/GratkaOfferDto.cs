fa03b0514f5cfb32c4f6c4b818009418e6e8e4d5
Mikolaj <mikolaj.bednarek@gmail.com>
2020-03-22 12:42:44 +0100

    added gratka integration template

---
+++ PropertyManagement.BLL/Dtos/GratkaOfferDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyManagement.BLL.Dtos
+{
+    public class GratkaOfferDto
+    {
+        public string City { get; set; }
+        public string StreetName { get; set; }
+        public string StreetNumber { get; set; }
+        public float Price { get; set; }
+        public string OwnerFirstName { get; set; }
+        public string OwnerEmail { get; set; }
+    }
+}
---
+++ PropertyManagement.BLL/Services/IGratkaOfferApi.cs
@@ -0,0 +1,13 @@
+﻿using PropertyManagement.BLL.Dtos;
+using PropertyManagement.BLL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyManagement.BLL.Services
+{
+    public interface IGratkaOfferApi
+    {
+        GratkaOfferDto CreateOffer(House house);
+    }
+}
